c27f3b7b10bd47de7041d5e71d1462d738a16aef
paisuygoda <paisuygoda3@gmail.com>
2020-10-03 16:15:18 +0900

    targetにsubjectが含まれるとき、resultの消去タイミングの都合で描画に問題があったのを修正

--- plugins/MyMiscPlugins.js
+++ plugins/MyMiscPlugins.js
@@ -294,4 +294,27 @@
 		this.gainDrainedHp(value);
 	};
 
+	// 計算ごとにresultを消去する処理を削除。どうせひとまとまりの計算ごとには消去されるのと、subjectのダメージ量を保持しておかないと描画に影響が出るため
+	Game_Action.prototype.apply = function(target) {
+		var result = target.result();
+		// this.subject().clearResult();
+		// result.clear();
+		result.used = this.testApply(target);
+		result.missed = (result.used && Math.random() >= this.itemHit(target));
+		result.evaded = (!result.missed && Math.random() < this.itemEva(target));
+		result.physical = this.isPhysical();
+		result.drain = this.isDrain();
+		if (result.isHit()) {
+			if (this.item().damage.type > 0) {
+				result.critical = (Math.random() < this.itemCri(target));
+				var value = this.makeDamageValue(target, result.critical);
+				this.executeDamage(target, value);
+			}
+			this.item().effects.forEach(function(effect) {
+				this.applyItemEffect(target, effect);
+			}, this);
+			this.applyItemUserEffect(target);
+		}
+	};
+
 })();
